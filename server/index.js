09088b011feb1dd79e08c94d3e01d631e434676f
jai-chawla <140626094+jai-chawla@users.noreply.github.com>
2023-12-26 22:25:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ dotenv.config();
 
 const app=express();
 
-app.use(cors());
+app.use(cors({
+  origin:["https://dall-e-client-three.vercel.app"],
+  methods:["POST", "GET"],
+  credentials: true
+}
+));
 app.use(express.json({limit:'50mb'}));
 
 app.use('/api/v1/post',postRoutes);
